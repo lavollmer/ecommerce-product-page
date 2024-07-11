3f2a20aebe3127814fd0979f0ee9940a75c5a68a
Laura <laura.e.vollmer@gmail.com>
2024-07-11 14:02:50 -0500

    added fontSize into tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,20 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        'kumbh': ['Kumbh-sans', 'sans-serif']
+        kumbh: ["Kumbh-sans", "sans-serif"],
       },
       colors: {
-        'orange': "hsl(26, 100%, 55%)",
+        orange: "hsl(26, 100%, 55%)",
         "pale-orange": "hsl(25, 100%, 94%)",
         "very-dark-blue": "hsl(220, 13%, 13%)",
         "dark-grayish-blue": "hsl(219, 9%, 45%)",
         "grayish-blue": "hsl(220, 14%, 75%)",
         "light-grayish-blue": "hsl(223, 64%, 98%)",
-        'white': "hsl(0, 0%, 100%)",
-        'black': "hsl(0, 0%, 0%)"
+        white: "hsl(0, 0%, 100%)",
+        black: "hsl(0, 0%, 0%)",
+      },
+      fontSize: {
+        "16px": "16px",
       },
     },
   },

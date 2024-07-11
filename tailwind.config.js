72c2e54a06c0de6618ffa4a26e37822f0d4de5cf
Laura <laura.e.vollmer@gmail.com>
2024-07-11 13:58:08 -0500

    added colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'orange': "hsl(26, 100%, 55%)",
+        "pale-orange": "hsl(25, 100%, 94%)",
+        "very-dark-blue": "hsl(220, 13%, 13%)",
+        "dark-grayish-blue": "hsl(219, 9%, 45%)",
+        "grayish-blue": "hsl(220, 14%, 75%)",
+        "light-grayish-blue": "hsl(223, 64%, 98%)",
+        'white': "hsl(0, 0%, 100%)",
+        'black': "hsl(0, 0%, 0%)"
+      },
+    },
   },
   plugins: [],
-}
+};

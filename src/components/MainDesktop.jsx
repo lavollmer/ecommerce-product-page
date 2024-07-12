847f11156fe2d247961e06d9e321bb85cd6ef93b
Laura <laura.e.vollmer@gmail.com>
2024-07-12 08:39:34 -0500

    created new component for main text and copy

---
+++ src/components/MainDesktop.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import sneakersMain from "../assets/image-product-1.jpg";
+
+const MainDesktop = () => {
+  return (
+    <div className="flex">
+      <div>
+        <img src={sneakersMain} alt="sneakers" />
+      </div>
+    </div>
+  );
+};
+
+export default MainDesktop;

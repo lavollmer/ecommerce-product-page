d67067c922d0a075b0e215c587d56fdc6e115b4c
Laura <laura.e.vollmer@gmail.com>
2024-07-15 13:00:30 -0500

    styling the dropdown based on design layout

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 const Dropdown = () => {
   return (
-    <ul className="absolute fixed top-14 left-0 w-48 bg-white shadow-md border border-gray-100">
-      <li className="px-4 py-2 hover:bg-gray-100">Item 1</li>
-      <li className="px-4 py-2 hover:bg-gray-100">Item 2</li>
-      <li className="px-4 py-2 hover:bg-gray-100">Item 3</li>
-    </ul>
+    <div className="absolute fixed top-14 left-0 w-48 p-4 bg-white shadow-md border border-gray-100">
+      <h1 className="font-kumbhs text-dark-grayish-blue font-bold">Cart</h1>
+      <hr />
+        <p>Product 1</p>
+        <button className="flex flex-row items-center bg-orange text-black font-bold text-lg rounded-lg py-3 px-6 w-full">Checkout</button>
+    </div>
   );
 };
 

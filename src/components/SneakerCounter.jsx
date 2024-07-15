299c9f892b34d0e17f520eb6b0b8223db5fbbdb1
Laura <laura.e.vollmer@gmail.com>
2024-07-15 09:43:59 -0500

    styling of counter button

--- src/components/SneakerCounter.jsx
+++ src/components/SneakerCounter.jsx
@@ -20,7 +20,10 @@ const SneakerCounter = () => {
         <input
           type="number"
           className="outline-none focus:outline-none text-center w-full bg-gray-300 font-semibold text-md hover:text-black focus:text-black md:text-basecursor-default flex items-center"
-        ></input>
+        />
+        <button data-action="increment" className="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer">
+            <span className="m-auto text-2xl font-thin">+</span>
+        </button>
       </div>
     </div>
   );

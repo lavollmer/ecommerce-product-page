db6fb727a275b767cc0384e460bba50361510c7b
Laura <laura.e.vollmer@gmail.com>
2024-07-15 11:59:45 -0500

    creating a carousel in the middle

--- src/components/CarouselImages.jsx
+++ src/components/CarouselImages.jsx
@@ -36,7 +36,7 @@ const CarouselImages = () => {
   };
 
   return (
-    <div className="flex flex-col bg-transparent z-50">
+    <div className="fixed top-0 right-0 bottom-0 left-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
       <img
         src={iconClose}
         alt="close icon"

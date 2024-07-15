d4e5b8b3395246e0ff63701b84f5889e932a94a2
Laura <laura.e.vollmer@gmail.com>
2024-07-15 11:35:46 -0500

    styling the images on the desktop

--- src/components/CarouselImages.jsx
+++ src/components/CarouselImages.jsx
@@ -1,50 +1,60 @@
-import React from 'react'
-import {useState} from 'react';
+import React from "react";
+import { useState } from "react";
 import ImageProductOne from "../assets/image-product-1.jpg";
 import ImageProductTwo from "../assets/image-product-2.jpg";
 import ImageProductThree from "../assets/image-product-3.jpg";
 import ImageProductFour from "../assets/image-product-4.jpg";
 
-
 const CarouselImages = () => {
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [isPoppedOut, setIsPoppedOut] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isPoppedOut, setIsPoppedOut] = useState(false);
 
-    const images =[
-'ImageProductOne',
-'ImageProductTwo',
-'ImageProductThree'
-, 'ImageProductFour'
-    ]
+  const images = [
+    ImageProductOne,
+    ImageProductTwo,
+    ImageProductThree,
+    ImageProductFour,
+  ];
 
-    const goToNext = () => {
-        setCurrentIndex((prevIndex) => {prevIndex + 1) % images.length);
-    };
+  const goToNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
 
-    const goToPrev = () => {
-        setCurrentIndex((prevIndex) => {prevIndex - 1 + images.length) % images.length);
-    };
+  const goToPrev = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + images.length) % images.length
+    );
+  };
 
-    const togglePopOut = () => {
-       setIsPoppedOut(!isPoppedOut);
-    };
+  const togglePopOut = () => {
+    setIsPoppedOut(!isPoppedOut);
+  };
 
   return (
-    <div className='flex items-center justify-center space-x-4'>
-        <button onClick={goToPrev}
-        className="p-2 bg-gray-200 rounded-full hover:bg-gray-300">&lt; </button>
- <img
+    <div className="flex items-center justify-center space-x-4 rounded-lg">
+      <button
+        onClick={goToPrev}
+        className="p-2 bg-gray-200 rounded-full hover:bg-gray-300"
+      >
+        &lt;{" "}
+      </button>
+      <img
         src={images[currentIndex]}
         alt="carousel"
         onClick={togglePopOut}
-        className={`transition-transform ${isPoppedOut ? 'scale-150' : 'scale-100'}`}
-        style={{ maxWidth: '90%', maxHeight: '75vh' }}
+        className={`transition-transform ${
+          isPoppedOut ? "scale-150" : "scale-100"
+        }` }
+        style={{ maxWidth: "90%", maxHeight: "75vh" }}
       />
-      <button onClick={goToNext} className='p-2 bg-gray-200 rounded-full hvoer:bg-gray-300'>
-      &gt;
+      <button
+        onClick={goToNext}
+        className="p-2 bg-gray-200 rounded-full hvoer:bg-gray-300"
+      >
+        &gt;
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default CarouselImages
+export default CarouselImages;
--- src/components/MainDesktop.jsx
+++ src/components/MainDesktop.jsx
@@ -1,11 +1,41 @@
 import React from "react";
 import sneakersMain from "../assets/image-product-1.jpg";
+import CarouselImages from "./CarouselImages";
+import ImageProductOne from "../assets/image-product-1.jpg";
+import ImageProductTwo from "../assets/image-product-2.jpg";
+import ImageProductThree from "../assets/image-product-3.jpg";
+import ImageProductFour from "../assets/image-product-4.jpg";
 
 const MainDesktop = () => {
   return (
-    <div className="flex flex-row p-20">
+    <div className="flex flex-col p-20 space-y-10">
       <div>
         <img src={sneakersMain} alt="sneakers image" className="rounded-lg" />
+      </div>
+      <div className="flex flex-row items-center space-x-20 ml-10">
+        <img
+          src={ImageProductOne}
+          alt="product one"
+          className="w-20 h-20 rounded-lg"
+        />
+        <img
+          src={ImageProductTwo}
+          alt="product two"
+          className="w-20 h-20 rounded-lg"
+        />
+        <img
+          src={ImageProductThree}
+          alt="product three"
+          className="w-20 h-20 rounded-lg"
+        />
+        <img
+          src={ImageProductFour}
+          alt="product four"
+          className="w-20 h-20 rounded-lg"
+        />
+      </div>
+      <div>
+        <CarouselImages />
       </div>
     </div>
   );

7b44283d36bfafa31be3657057b8d61e24fa2c32
Laura <laura.e.vollmer@gmail.com>
2024-07-15 12:50:32 -0500

    adding a dropdown for the cart

--- src/components/DesktopNav.jsx
+++ src/components/DesktopNav.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { useState } from "react";
 import logo from "../assets/logo.svg"; // Import logo from assets folder
 import cart from "../assets/icon-cart.svg"; // Import cart from assets folder
 import avatar from "../assets/image-avatar.png"; // Import avatar from assets folder
 
 const DesktopNav = () => {
+  const [showDropdownCart, setShowDropdownCart] = useState(false);
+
+  const handleCartClick = () => {
+    setShowDropdownCart(true);
+  };
+
   return (
     <div className="fixed top-0 left-0 w-full font-kumbh">
       <div className="flex flex-row justify-evenly items-center">
@@ -16,15 +23,33 @@ const DesktopNav = () => {
             <li className="border-b border-transparent hover:border-orange transition-colors duration-300 inline-block">
               Collections
             </li>
-            <li className="border-b border-transparent hover:border-orange transition-colors duration-300 inline-block">Men</li>
-            <li className="border-b border-transparent hover:border-orange transition-colors duration-300 inline-block">Women</li>
-            <li className="border-b border-transparent hover:border-orange transition-colors duration-300 inline-block">About</li>
-            <li className="border-b border-transparent hover:border-orange transition-colors duration-300 inline-block">Contact</li>
+            <li className="border-b border-transparent hover:border-orange transition-colors duration-300 inline-block">
+              Men
+            </li>
+            <li className="border-b border-transparent hover:border-orange transition-colors duration-300 inline-block">
+              Women
+            </li>
+            <li className="border-b border-transparent hover:border-orange transition-colors duration-300 inline-block">
+              About
+            </li>
+            <li className="border-b border-transparent hover:border-orange transition-colors duration-300 inline-block">
+              Contact
+            </li>
           </ul>
         </div>
         <div className="flex flex-row items-center mt-10 ml-10 space-x-6">
-          <img src={cart} alt="cart icon" className="cursor-pointer" />
-          <img src={avatar} alt="avatar" className="h-10 w-10 cursor-pointer transition-colors duration-300 border-transparent b-2 hover:border-orange-800" />
+          <img
+            src={cart}
+            onClick={handleCartClick}
+            alt="cart icon"
+            className="cursor-pointer"
+          />
+          <img
+            src={avatar}
+            onClick={handleCartClick}
+            alt="avatar icon"
+            className="h-10 w-10 cursor-pointer transition-colors duration-300 border-transparent b-2 hover:border-orange-800"
+          />
         </div>
       </div>
       <hr className="mt-10" />
---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Dropdown = () => {
+  return (
+    <div>
+        <ul>
+            <li>Hello</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Dropdown

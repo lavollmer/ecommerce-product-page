337c7e530e2a3899470c87b54fae47ddbf54ea23
Laura <laura.e.vollmer@gmail.com>
2024-07-15 11:39:07 -0500

    updating the images on the desktop for styling purposes

--- src/components/MainDesktop.jsx
+++ src/components/MainDesktop.jsx
@@ -9,34 +9,34 @@ import ImageProductFour from "../assets/image-product-4.jpg";
 const MainDesktop = () => {
   return (
     <div className="flex flex-col p-20 space-y-10">
-      <div>
-        <img src={sneakersMain} alt="sneakers image" className="rounded-lg" />
+      <div className="flex items-center justify-center">
+        <img src={sneakersMain} alt="sneakers image" className="rounded-lg h-100 w-100" />
       </div>
-      <div className="flex flex-row items-center space-x-20 ml-10">
+      <div className="flex flex-row items-center justify-between">
         <img
           src={ImageProductOne}
           alt="product one"
-          className="w-20 h-20 rounded-lg"
+          className="w-40 h-40 rounded-lg"
         />
         <img
           src={ImageProductTwo}
           alt="product two"
-          className="w-20 h-20 rounded-lg"
+          className="w-40 h-40 rounded-lg"
         />
         <img
           src={ImageProductThree}
           alt="product three"
-          className="w-20 h-20 rounded-lg"
+          className="w-40 h-40 rounded-lg"
         />
         <img
           src={ImageProductFour}
           alt="product four"
-          className="w-20 h-20 rounded-lg"
+          className="w-40 h-40 rounded-lg"
         />
       </div>
-      <div>
+      {/* <div>
         <CarouselImages />
-      </div>
+      </div> */}
     </div>
   );
 };

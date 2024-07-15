9793f332ed3302c1b213f447a237034e9071414a
Laura <laura.e.vollmer@gmail.com>
2024-07-15 08:57:11 -0500

    working on desktop layout with pricing and styling

--- src/components/MainDesktop.jsx
+++ src/components/MainDesktop.jsx
@@ -5,7 +5,7 @@ const MainDesktop = () => {
   return (
     <div className="flex flex-row p-20">
       <div>
-        <img src={sneakersMain} alt="sneakers" />
+        <img src={sneakersMain} alt="sneakers image" className="rounded-lg" />
       </div>
     </div>
   );
--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Text = () => {
   return (
-    <div>
-        <h1>SNEAKER COMPANY</h1>
-        <h2>Fall Limited Edition Sneakers</h2>
-        <h3>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.</h3>
+    <div className="flex flex-col flex-start p-20 space-y-6 font-kumbh">
+      <h1 className="text-dark-grayish-blue font-bold">SNEAKER COMPANY</h1>
+      <h2 className="text-black font-bold text-4xl">
+        Fall Limited Edition Sneakers
+      </h2>
+      <h3 className="text-dark-grayish-blue">
+        These low-profile sneakers are your perfect casual wear companion.
+        Featuring a durable rubber outer sole, they'll withstand everything the
+        weather can offer.
+      </h3>
+      <div className="flex flex-row space-x-4">
+        <h3 className="font-bold text-black text-2xl">$125.00</h3>
+        <h3 className="flex flex-row items-center font-bold bg-black rounded-lg text-white">50%</h3>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Text
+export default Text;

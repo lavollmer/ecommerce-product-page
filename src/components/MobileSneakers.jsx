c849865603a584755870207c8cf8e5482c6b9bc1
Laura <laura.e.vollmer@gmail.com>
2024-07-17 08:14:40 -0500

    updating the carousel of images for mobile to fit within the application and not on top of

--- src/components/MobileSneakers.jsx
+++ src/components/MobileSneakers.jsx
@@ -41,16 +41,9 @@ const CarouselImages = () => {
   }
 
   return (
-    <div className="fixed top-0 right-0 bottom-0 left-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+    <div className="flex flex-col p-10 flex items-center justify-center">
       <div>
-        <div className="relative flex items-center justify-center space-x-4 rounded-lg">
-          <img
-            src={iconClose}
-            alt="close icon"
-            className="absolute top-0 right-0 text-orange cursor-pointer z-50"
-            onClick={closeCarousel}
-            style={{ width: "24px", height: "24px" }}
-          />
+        <div className="flex items-center justify-center space-x-4 rounded-lg">
           <div className="flex items-center justify-center space-x-4 rounded-lg">
             <button
               onClick={goToPrev}
@@ -61,9 +54,8 @@ const CarouselImages = () => {
             <img
               src={images[currentIndex]}
               alt="carousel images"
-              // onClick={togglePopOut}
               className={`transition-transform ${
-                isPoppedOut ? "scale-150" : "scale-100"
+                isPoppedOut ? "scale-100" : "scale-100"
               }`}
               style={{
                 maxWidth: "90%",

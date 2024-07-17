39834714aee895a114c009c28e9686702bada6de
Laura <laura.e.vollmer@gmail.com>
2024-07-17 11:16:38 -0500

    reverting back to old design

--- src/components/AddToCart.jsx
+++ src/components/AddToCart.jsx
@@ -1,17 +1,23 @@
 import React from "react";
-import PropTypes from "prop-types";
 import cart from "../assets/icon-cart.svg";
 
 const AddToCart = ({ setCount }) => {
+
+  const handleAddToCart = () => {
+    setCount((prevCount) => prevCount + 1); // Increment count
+  };
+
   return (
     <div className="flex flex-row w-full h-full items-center justify-center font-kumbh font-bold font-lg">
-      <button onClick={() => setCount((prevCount) => prevCount + 1)} className="flex flex-row items-center bg-orange text-black rounded-lg py-6 px-16" >
+      <button
+        onClick={handleAddToCart}
+        className="flex flex-row items-center bg-orange text-black rounded-lg py-6 px-16"
+      >
         <img src={cart} alt="cart icon" className="mr-2" />
         <p className="font-bold">Add to Cart</p>
       </button>
     </div>
   );
 };
-
 
 export default AddToCart;
--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -3,14 +3,8 @@ import { useState } from "react";
 import ThumbnailImageProductOne from "../assets/image-product-1-thumbnail.jpg";
 import iconTrash from "../assets/icon-delete.svg";
 
-const Dropdown = ({ cartCount}) => {
+const Dropdown = ({ cartCount }) => {
   const [total, setTotal] = useState(125.0 * cartCount);
-
-  const [cartItems, setCartItems] = useState([    ]);
-
-  const clearCart = () => {
-    setCartItems([]);
-  };
 
   return (
     <div className="absolute fixed top-14 left-0 w-full bg-white shadow-md border border-gray-100">
@@ -18,7 +12,6 @@ const Dropdown = ({ cartCount}) => {
         Cart
       </h1>
       <hr />
-      {cartItems.length > 0 ? (
       <div className="flex flex-row items-center space-x-4 m-2">
         <div className="flex flex-col">
           <img
@@ -27,7 +20,7 @@ const Dropdown = ({ cartCount}) => {
             className="h-14 w-16 rounded-lg"
           />
         </div>
-        <div >
+        <div>
           <div className="font-kumbhs text-md text-grayish-blue">
             <h2>Fall Limited Edition Sneakers</h2>
           </div>
@@ -43,11 +36,6 @@ const Dropdown = ({ cartCount}) => {
           className="h-4 w-3 cursor-pointer"
         />
       </div>
-      ) : (
-        <div className="flex flex-row items-center justify-center p-4">
-          <p className="font-kumbhs text-md text-grayish-blue">Your cart is empty.</p>
-        </div>
-      )}
       <div className="flex flex-col p-6">
         <button className="flex flex-row items-center justify-center bg-orange text-black font-bold text-lg rounded-lg py-2 px-4 w-full">
           Checkout
